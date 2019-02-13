a96cf795fac836c86b2eaeece8a184cdc53d6837
Lenon <lenonmauer@gmail.com>
2019-02-13 17:25:24 -0200

    added search route

--- start/routes.js
+++ start/routes.js
@@ -12,7 +12,6 @@ Route.get('/file/:id', 'FileController.show');
 
 Route.group(() => {
   Route.get('/profile', 'UserController.show');
-
   Route.put('/users', 'UserController.update').validator('User/Update');
 
   Route.post('/upload', 'FileController.store');
@@ -22,6 +21,8 @@ Route.group(() => {
   Route.get('/meetups', 'MeetupController.index');
   Route.get('/meetups/:id', 'MeetupController.show');
   Route.post('/meetups', 'MeetupController.store');
+
+  Route.get('/meetups-search', 'SearchController.index');
 
   Route.post('/subscriptions', 'SubscriptionController.store');
 }).middleware(['auth']);
