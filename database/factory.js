7e8e8e8d5d91bcf7bf463c29f3eb6b8dd679f0a0
Lenon <lenonmauer@gmail.com>
2019-02-19 12:47:31 -0300

    added fakes for user and category

--- database/factory.js
+++ database/factory.js
@@ -1,21 +1,17 @@
 'use strict';
 
-/*
-|--------------------------------------------------------------------------
-| Factory
-|--------------------------------------------------------------------------
-|
-| Factories are used to define blueprints for database tables or Lucid
-| models. Later you can use these blueprints to seed your database
-| with dummy data.
-|
-*/
+const Factory = use('Factory');
 
-/** @type {import('@adonisjs/lucid/src/Factory')} */
-// const Factory = use('Factory');
+Factory.blueprint('App/Models/User', async (faker, i, data) => {
+  return {
+    name: faker.name(),
+    email: faker.email(),
+    password: faker.string({ length: 8 }),
+  };
+});
 
-// Factory.blueprint('App/Models/User', (faker) => {
-//   return {
-//     username: faker.username()
-//   }
-// })
+Factory.blueprint('App/Models/Category', async (faker, i, data) => {
+  return {
+    name: faker.name(),
+  };
+});
